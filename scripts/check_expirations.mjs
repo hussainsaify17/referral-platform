9e000bf0078335f4b35fcdbf1e0d5521d37c237f
Hussain Saify <itssaify.17@live.com>
2026-07-04 11:34:26 +0530

    feat: change check_expirations update status to expired to match updated sheet dropdown

--- scripts/check_expirations.mjs
+++ scripts/check_expirations.mjs
@@ -78,7 +78,7 @@ async function run() {
                 type: "update_referral",
                 slug: row.slug,
                 updates: {
-                  status: "not active"
+                  status: "expired"
                 }
               }),
             });
